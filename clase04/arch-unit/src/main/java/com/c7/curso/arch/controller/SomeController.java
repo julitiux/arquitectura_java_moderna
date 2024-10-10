443ebfb5a7116fb68f6b4ed55bb35813c84b2903
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-10 13:50:46 -0600

    instanciando clase SomeService para usar el emtodo sayHi dentro de methodOne

--- clase04/arch-unit/src/main/java/com/c7/curso/arch/controller/SomeController.java
+++ clase04/arch-unit/src/main/java/com/c7/curso/arch/controller/SomeController.java
@@ -1,4 +1,13 @@
 package com.c7.curso.arch.controller;
 
+import com.c7.curso.arch.service.SomeService;
+
 public class SomeController {
+
+  private SomeService someService;
+
+  public void methodOne(){
+    someService.sayHi();
+  }
+
 }
