3e725faf22786bbda5da9945b62946d3b50e66da
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-10 14:00:25 -0600

    se geenra instancia de SomeRepository

--- clase04/arch-unit/src/main/java/com/c7/curso/arch/service/SomeService.java
+++ clase04/arch-unit/src/main/java/com/c7/curso/arch/service/SomeService.java
@@ -1,9 +1,17 @@
 package com.c7.curso.arch.service;
 
+import com.c7.curso.arch.repository.SomeRepository;
+
 public class SomeService {
+
+  private SomeRepository someRepository;
 
   public void sayHi(){
     System.out.println("Hi from Service");
+  }
+
+  public void sayNo(){
+    someRepository.save();
   }
 
 
