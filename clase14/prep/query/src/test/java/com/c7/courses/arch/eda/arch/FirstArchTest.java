3ea6cbd4dc24096946856224942b522f2a6870a7
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-11-07 23:37:32 -0600

    agregando prueba para asegurar acomplamiento debil entre capas

--- clase14/prep/query/src/test/java/com/c7/courses/arch/eda/arch/FirstArchTest.java
+++ clase14/prep/query/src/test/java/com/c7/courses/arch/eda/arch/FirstArchTest.java
@@ -3,6 +3,7 @@ package com.c7.courses.arch.eda.arch;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,17 @@ public class FirstArchTest {
         ArchRule rule = SlicesRuleDefinition.slices()
             .matching("com.c7.(*)..")
             .should().beFreeOfCycles();
+
+        rule.check(classes);
+    }
+
+    @Test
+    public void domain_should_not_depend_on_infrastructure() {
+        JavaClasses classes = new ClassFileImporter().importPackages("com.c7");
+
+        ArchRule rule = ArchRuleDefinition.noClasses()
+            .should().dependOnClassesThat()
+            .resideInAPackage("..infrastructure..");
 
         rule.check(classes);
     }
