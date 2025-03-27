6a9ed6522bf830b8c7656d349ea33aeced302f24
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-03-26 18:49:04 -0600

    add princial project management

---
+++ src/main/java/com/arquitectura_java_moderna/ArquitecturaJavaModernaApplication.java
@@ -0,0 +1,13 @@
+package com.arquitectura_java_moderna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaJavaModernaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaJavaModernaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arquitectura_java_moderna/ArquitecturaJavaModernaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitectura_java_moderna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaJavaModernaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
